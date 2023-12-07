9e0bae5df3fe26f29c1ae386589eb6059638d65a
Brian Colclough <brianmcolclough@gmail.com>
2023-12-07 14:09:31 -0500

    master - vercel analytics

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,16 @@
 import {defineConfig} from 'astro/config';
+import vercel from '@astrojs/vercel/serverless';
 
 import solidJs from "@astrojs/solid-js";
 
 // https://astro.build/config
 export default defineConfig({
+    output: 'server',
+    adapter: vercel({
+        webAnalytics: {
+            enabled: true,
+        },
+    }),
     integrations: [solidJs()],
     prefetch: true
 });
