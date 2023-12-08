120fa75c7b239797cf9f7c9a2885a2f13475a679
Brian Colclough <brianmcolclough@gmail.com>
2023-12-07 19:10:37 -0500

    master - removes vercel analytics

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,8 @@
 import {defineConfig} from 'astro/config';
-import vercel from '@astrojs/vercel/serverless';
 import solidJs from "@astrojs/solid-js";
 
 // https://astro.build/config
 export default defineConfig({
-    output: 'server',
-    adapter: vercel({
-        webAnalytics: {
-            enabled: true,
-        },
-    }),
     integrations: [solidJs()],
     prefetch: true
 });
