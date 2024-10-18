bcc55698b250c57a384de90fa0f2ffac3fdae2ec
Brian Colclough <brianmcolclough@gmail.com>
2024-10-18 15:52:04 -0400

    [WIP] Adding contentfulCMS
    - adds contentfulCMS as dependency
    - adds type for blog posts

--- src/components/card/card.tsx
+++ src/components/card/card.tsx
@@ -1,52 +1,54 @@
 import { Show } from "solid-js";
 import "./card.css";
+import type { EntryFieldTypes } from "contentful";
+import { documentToHtmlString } from "@contentful/rich-text-html-renderer";
 
 export type CardProps = {
-  title?: string;
-  subTitle?: string;
-  body?: string;
-  linkTo?: string;
-  tags?: string[];
+    title: string;
+    body: string;
+    subTitle: string;
+    linkTo?: string;
+    tags?: string[];
 };
 
 enum CardClass {
-  Card = "card",
-  CardTitle = "card-title",
-  CardSubTitle = "card-subtitle",
-  CardBody = "card-body",
-  CardTags = "card-tags",
+    Card = "card",
+    CardTitle = "card-title",
+    CardSubTitle = "card-subtitle",
+    CardBody = "card-body",
+    CardTags = "card-tags",
 }
 
 export const Card = (props: CardProps) => {
-  const { title, subTitle, body, linkTo, tags } = props;
+    const { title, subTitle, body, linkTo, tags } = props;
 
-  return (
-    <div class={CardClass.Card}>
-      <Show when={title}>
-        <h1 class={[CardClass.CardTitle, "wubberly"].join(" ")}>
-          <a href={linkTo}> {title}</a>
-        </h1>
-      </Show>
-      <Show when={subTitle}>
-        <i>
-          <h2 class={[CardClass.CardSubTitle].join(" ")}>{subTitle}</h2>
-        </i>
-      </Show>
-      <Show when={body}>
-        <p class={CardClass.CardBody}>{body}</p>
-      </Show>
-      <Show when={tags}>
-        <div class={CardClass.CardTags}>
-          <sub class="tags-list">
-            {tags?.map((tag) => (
-              <>
-                <strong>{tag}</strong>
-                <span class="tag-separator">/</span>
-              </>
-            ))}
-          </sub>
+    return (
+        <div class={CardClass.Card}>
+            <Show when={title}>
+                <h1 class={[CardClass.CardTitle, "wubberly"].join(" ")}>
+                    <a href={linkTo}>{title.toString()}</a>
+                </h1>
+            </Show>
+            <Show when={subTitle}>
+                <i>
+                    <time class={CardClass.CardSubTitle}>{subTitle}</time>
+                </i>
+            </Show>
+            <Show when={body}>
+                <article class={CardClass.CardBody}>{body}</article>
+            </Show>
+            <Show when={tags}>
+                <div class={CardClass.CardTags}>
+                    <sub class="tags-list">
+                        {tags?.map((tag) => (
+                            <>
+                                <strong>{tag}</strong>
+                                <span class="tag-separator">/</span>
+                            </>
+                        ))}
+                    </sub>
+                </div>
+            </Show>
         </div>
-      </Show>
-    </div>
-  );
+    );
 };
