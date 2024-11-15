96226d9a3d546b2816c5da3311c4ff772e35f7f6
Brian Colclough <brianmcolclough@gmail.com>
2024-11-15 11:35:23 -0500

    oops - adds sitemap install and import in astro.config

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,8 @@ import { defineConfig } from 'astro/config';
 
 import tailwind from "@astrojs/tailwind";
 
+import sitemap from "@astrojs/sitemap";
+
 // https://astro.build/config
 export default defineConfig({
   integrations: [solidJs(), tailwind(), sitemap()],
